8cb6cc0da800032dd32d614e25def8fc9fc80efa
Meerim Asylbekova <meerim.asylbekova@gmail.com>
2022-11-14 18:01:09 +0100

    Added very basic movie index and show pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,16 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show]
+
+  def index
+    @movies = Movie.all
+  end
+
   def show
-    @test_review = PolyReviewTest.new()
+  end
+
+  private
+
+  def set_movie
     @movie = Movie.find(params[:id])
   end
 end

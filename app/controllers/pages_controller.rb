0d2a16e11ef595e902a903987f7ea2deb934cd6e
sebastian navarro <sebastiannavarro.fl@gmail.com>
2022-11-15 12:47:33 +0100

    data imported for homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,60 @@
+require "json"
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :set_parses, only: %i[home]
 
   def home
+    @airly_tvs = set_tvs(@t_airly_tvs)
+    @popular_tvs = set_tvs(@t_popular_tvs)
+    @toprated_tvs = set_tvs(@t_toprated_tvs)
+    @weeklytrends_tvs = set_tvs(@t_weeklytrends_tvs)
+    @popular_movies = set_movies(@t_popular_movies)
+    @toprated_movies = set_movies(@t_toprated_movies)
+    @weeklytrends_movies = set_movies(@t_weeklytrends_movies)
+    @upcoming_movies = set_movies(@t_upcoming_movies)
   end
+
+  private
+
+  def set_movies(movies)
+    movies.map do |movie|
+      movie = {
+        title: movie["title"],
+        overview: movie["overview"],
+        poster_url: "https://image.tmdb.org/t/p/w780#{movie["poster_path"]}",
+        backdrop_url: "https://image.tmdb.org/t/p/w1280#{movie["backdrop_path"]}",
+        rating_average: movie["vote_average"],
+        tmdb_id: movie["id"],
+        release_date: movie["release_date"]
+      }
+    end
+  end
+
+  def set_tvs(tvs)
+    tvs.map do |tv|
+      movie = {
+        title: tv["name"],
+        overview: tv["overview"],
+        poster_url: "https://image.tmdb.org/t/p/w780#{tv["poster_path"]}",
+        backdrop_url: "https://image.tmdb.org/t/p/w1280#{tv["backdrop_path"]}",
+        rating_average: tv["vote_average"],
+        tmdb_id: tv["id"],
+        country: tv["origin_country"],
+        first_air_date: tv["first_air_date"]
+      }
+    end
+  end
+
+  def set_parses
+    @t_airly_tvs = JSON.parse(File.read("t_ontheair_tvs.json"))["results"]
+    @t_popular_tvs = JSON.parse(File.read("t_popular_tvs.json"))["results"]
+    @t_toprated_tvs = JSON.parse(File.read("t_toprated_tvs.json"))["results"]
+    @t_weeklytrends_tvs = JSON.parse(File.read("t_weeklytrends_tvs.json"))["results"]
+    @t_popular_movies = JSON.parse(File.read("t_popular_movies.json"))["results"]
+    @t_toprated_movies = JSON.parse(File.read("t_toprated_movies.json"))["results"]
+    @t_weeklytrends_movies = JSON.parse(File.read("t_weeklytrends_movies.json"))["results"]
+    @t_upcoming_movies = JSON.parse(File.read("t_upcoming_movies.json"))["results"]
+  end
+
 end

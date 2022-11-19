7a57921aa185f2a6dabef1d248df825226cd4856
sebastian navarro <sebastiannavarro.fl@gmail.com>
2022-11-19 16:15:36 +0100

    before try add migrations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,7 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show]
 
@@ -7,16 +11,28 @@ class MoviesController < ApplicationController
   end
 
   def show
+    watchmode_info = api_call("https://api.watchmode.com/v1/search/?apiKey=#{ENV['WATCHMODE_API_KEY']}&search_field=tmdb_movie_id&search_value=#{@movie.tmdb_id.to_s}")
+
+    imdb_id = watchmode_info["title_results"][0]["imdb_id"]
+    watchmode_id = watchmode_info["title_results"][0]["id"]
     @review = Review.new
     @reviewable = @movie
     @list_item = ListItem.new
     @listable = @movie
     @lists = List.where(["user_id = :user_id", { user_id: current_user.id }])
+    @providers
+    raise
   end
 
   private
 
   def set_movie
     @movie = Movie.find(params[:id])
+  end
+
+  def api_call(url)
+    uri = URI(url)
+    json = Net::HTTP.get(uri)
+    result = JSON(json)
   end
 end

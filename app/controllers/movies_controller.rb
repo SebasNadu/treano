4d1e3048d7cdaf87f2b1cc4762faa4944ded6b22
sebastian navarro <sebastiannavarro.fl@gmail.com>
2022-11-22 08:38:05 +0100

    providers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,16 +11,12 @@ class MoviesController < ApplicationController
   end
 
   def show
-     #watchmode_info = api_call("https://api.watchmode.com/v1/search/?apiKey=#{ENV['WATCHMODE_API_KEY']}&search_field=tmdb_movie_id&search_value=#{@movie.tmdb_id.to_s}")
-
-     #imdb_id = watchmode_info["title_results"][0]["imdb_id"]
-     #watchmode_id = watchmode_info["title_results"][0]["id"]
     @review = Review.new
     @reviewable = @movie
     @list_item = ListItem.new
     @listable = @movie
     @lists = List.where(["user_id = :user_id", { user_id: current_user.id }])
-    @providers
+    #raise
   end
 
   private
@@ -29,9 +25,4 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
-  def api_call(url)
-    uri = URI(url)
-    json = Net::HTTP.get(uri)
-    result = JSON(json)
-  end
 end
